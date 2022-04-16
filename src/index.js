4dd29de113797af3a6d4997ac8bae423fcefa4ad
aksinghrajpoot <imaksinghrajpoot@gmail.com.com>
2022-04-16 14:03:29 +0530

    removed spinner

--- src/index.js
+++ src/index.js
@@ -4,30 +4,11 @@ import Hero from "./Components/Hero"
 import Navbar from "./Components/Navbar"
 import Footer from "./Components/Footer"
 import Card from "./Components/CardData"
-import React, {useState,useEffect} from 'react';
 
 
 
 export default function App() {
-    const [isLoading, setLoading] = useState(true);
-
-    function fakeRequest() {
-      return new Promise(resolve => setTimeout(() => resolve(), 2500));
-    }
-  
-    useEffect(() => {
-      fakeRequest().then(() => {
-        const el = document.querySelector(".loader-container");
-        if (el) {
-          el.remove();
-          setLoading(!isLoading);
-        }
-      });
-    }, []);
-  
-    if (isLoading) {
-      return null;
-    }
+   
     return <>
         <Navbar />
         <Hero />
